72aef71b5acf072eda3af2bc4d73f00dadca3d8e
citanantaindrawan <citanantaindrawan@gmail.com>
2025-12-17 00:16:29 +0800

    add promise function to learn using promise

--- src/scripts.js
+++ src/scripts.js
@@ -43,3 +43,61 @@ const displayData = (data) => {
        `
     };
 }
+
+// this is the function that we use to fetch data and call function using promise
+const getDataPromise = async () => {
+    const keyword = document.getElementById("keywordInputPromise").value;
+    const url = "https://jsonplaceholder.typicode.com/users?id=" + keyword;
+    if (!keyword) {
+        alert("Please enter a keyword");
+    }
+    const loadingMsg = document.getElementById("loadingMsgPromise");
+    loadingMsg.removeAttribute("hidden");
+    // we wait for the fetchDataPromise function to finish
+    const data = await fetchDataPromise(url);
+    displayDataPromise(data);
+}
+
+// this is the function that we use to fetch data and call function using promise
+fetchDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        // set Timeout for 5000 ms / 5 seconds otherwise it will throw error. This simulate if later we handle the API with very big data and need to wait for more than 5 seconds to get the response it will throw error
+        setTimeout(() => {
+            if (url) {
+                // we fetch the data from the API below
+                fetch(url)
+                    // we convert the response to json
+                    .then((response) => response.json())
+                    // we resolve the data
+                    .then((data) => resolve(data))
+                    // we reject the error
+                    .catch((error) => reject(error))
+            } else {
+                reject("URL is not provided");
+            }
+        }, 5000)
+    })
+}
+
+
+// this is a function that use for display the data in the table
+const displayDataPromise = (data) => {
+    const result = document.getElementById("resultAreaPromise");
+    const resultTableHead = document.getElementById("resultTableHeadPromise");
+    const resultTableBody = document.getElementById("resultTableBodyPromise");
+    resultTableHead.removeAttribute("hidden");
+    resultTableBody.innerHTML = "";
+    for (items of data) {
+        resultTableBody.innerHTML = resultTableBody.innerHTML + `
+       <tr>
+            <td>${items.id}</td>
+            <td>${items.name}</td>
+            <td>${items.username}</td>
+            <td>${items.email}</td>
+            <td>${items.phone}</td>
+            <td>${items.website}</td>
+        </tr>
+       `
+    };
+}
+
